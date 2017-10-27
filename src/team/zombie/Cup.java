9096bd9d9ff61898d5e767c056bde556bf26abb2
Silver-Talen <silver_talen@hotmail.com>
2017-10-27 11:30:37 -0600

    cup update

--- src/team/zombie/Cup.java
+++ src/team/zombie/Cup.java
@@ -1,4 +1,33 @@
 package team.zombie;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Cup {
+    public Cup() {
+    }
+
+    public String DiceType() {
+        int percent1 = ThreadLocalRandom.current().nextInt(1, 101);
+        new String();
+        String Dice;
+        if (percent1 < 46) {
+            Dice = "Green";
+        } else if (percent1 > 77) {
+            Dice = "Red";
+        } else {
+            Dice = "Yellow";
+        }
+
+        return Dice;
+    }
+    public class CupReturn {
+        public CupReturn() {
+        }
+
+        public void main(String[] args) {
+            Cup DiceOutput = new Cup();
+            DiceOutput.DiceType();
+            System.out.println(DiceOutput.DiceType());
+        }
+    }
 }
