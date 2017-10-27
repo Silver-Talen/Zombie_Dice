1c28e2b2e73d67a9ff75d57e54215544f3d804df
Silver-Talen <silver_talen@hotmail.com>
2017-10-27 11:30:37 -0600

    cup update

--- Zombie Dice (Cup)/src/ZombieDice/CupReturn.java
+++
@@ -1,9 +0,0 @@
-package ZombieDice;
-
-public class CupReturn {
-    public static void main(String[] args) {
-        Cup DiceOutput = new Cup();
-        DiceOutput.DiceType();
-        System.out.println(DiceOutput.DiceType());
-    }
-}
--- src/team/zombie/Main.java
+++ src/team/zombie/Main.java
@@ -3,6 +3,6 @@ package team.zombie;
 public class Main {
 
     public static void main(String[] args) {
-        
+
     }
 }
