1ac593eed1d22497e344cfbd3a41c544e7ea58f4
Dean <deanapesek99@gmail.com>
2017-10-30 12:58:31 -0600

    game

--- src/team/zombie/Play_Game.java
+++ src/team/zombie/Play_Game.java
@@ -27,8 +27,11 @@ class Play_Game {
 
 
             Player_Sheet Player1 = new Player_Sheet();
-
+            Player_Sheet Player2 = new Player_Sheet();
             Dice Dice = new Dice();
+            String Brains = "Brains";
+            String Blast = "Blast";
+            String Footprints = "Footprints";
 
             String p1 = "";
             String p2 = "";
@@ -37,34 +40,89 @@ class Play_Game {
             Player1.setName(p1, 1);
             System.out.println("What is the name of player 2:");
             p2 = scanner.nextLine();
-            //send this data to the player class to store
-            //call default player data
+            Player2.setName(p2, 2);
 
-            boolean gameRunning;
-            boolean alive;
+            boolean gameRunning =true;
+            boolean alive =true;
             while (gameRunning) {
                 //make it display the stats for each player
 
-                System.out.println(Player1);
+
                 do {
+                    System.out.println(Player1.getName());
+                    System.out.println(Player1.getBrainsEaten());
+                    System.out.println(Player1.getShots());
+                    System.out.println(Player1.getSurvivorsCornered());
 
+                    System.out.println(Dice.getResult());
+                    System.out.println(Dice.getResult());
+                    System.out.println(Dice.getResult());
+                    //set dice.Results = player stats
 
-                    while (alive) {
-
-                        Dice.dice();
-
-                        //each individual turn for the player
-                        //call dice/cup for the dice rolls
-                        //then set/change stats for player
-                        //Runs til they say they want to stop or they get shot 3 times
-
+                    for (int i = 1; i < 3; i++) {
+                        if (Dice.result.equals(Brains)) {
+                            Player1.setBrainsEaten(+1);
+                        }
+                        else if (Dice.result.equals(Blast)) {
+                            Player1.setShots(+1);
+                        }
+                        else if (Dice.result.equals(Footprints)) {
+                            Dice.fpKeep = true;
+                        }
+                    }
+                    if (Player1.getShots() >= 3) {
+                        alive = false;
                     }
 
-                    //til player wins continue
+                    System.out.println("Would you like to continue? Y/N");
+
+
+                    }while (alive);
+
+                do {
+                    System.out.println(Player2.getName());
+                    System.out.println(Player2.getBrainsEaten());
+                    System.out.println(Player2.getShots());
+                    System.out.println(Player2.getSurvivorsCornered());
+
+                    System.out.println(Dice.getResult());
+                    System.out.println(Dice.getResult());
+                    System.out.println(Dice.getResult());
+                    //Keep footprint dice
+
+                    for (int i = 1; i < 3; i++) {
+                        if (Dice.result.equals(Brains)) {
+                            Player2.setBrainsEaten(+1);
+                        }
+                        else if (Dice.result.equals(Blast)) {
+                            Player2.setShots(+1);
+                        }
+                        else if (Dice.result.equals(Footprints)) {
+                            Dice.fpKeep = true;
+                        }
+                    }
+                    if (Player2.getShots() >= 3) {
+                        alive = false;
+                    }
+
+                    System.out.println("Would you like to continue? Y/N");
+
+
+                }while (alive);
+
+
+
+
+                //til player wins continue
+
+
+
+
+
+
+
 
                 }
-            }
-
         }while (keepPlaying);
     }
 
