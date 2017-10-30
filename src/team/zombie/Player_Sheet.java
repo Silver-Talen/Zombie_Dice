1af215fdc9aa9536f571005be620adef585eed34
Tanner <TStrange@neumont.student.edu>
2017-10-30 10:55:16 -0600

    game

--- src/team/zombie/Player_Sheet.java
+++ src/team/zombie/Player_Sheet.java
@@ -2,8 +2,30 @@ package team.zombie;
 
 class Player_Sheet {
 
-    int shots = 0;
-    int brains = 0;
-    int cornered = 0;
+    static void player() {
+        int Shots = 0;
+        int BrainsEatenP1 = 0;
+        int BrainsEatenP2 = 0;
+        int SurvivorsCornered = 0;
+        boolean turn = true;
+        boolean turntwo = true;
 
+        while (turn) {
+            //Player 1
+            System.out.println(p1);
+            System.out.println("Shots: " + Shots);
+            System.out.println("Survivors Cornered: " + SurvivorsCornered);
+
+            System.out.println("Brains Eaten: " + BrainsEatenP1);
+        }
+//______________________________________________________________________________________________
+        while (turntwo) {
+            //Player 2
+            System.out.println(p2);
+            System.out.println("Shots: " + Shots);
+            System.out.println("Survivors Cornered: " + SurvivorsCornered);
+
+            System.out.println("Brains Eaten: " + BrainsEatenP2);
+        }
+    }
 }
